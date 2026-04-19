a13cebec888be8a85aa3e4b8e67df711908e65bc
Uki Hong <suhong9507@gmail.com>
2026-04-19 20:12:04 +0100

    add basic .env configuration and example env file

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
+import os
+
+# to bring os.getenv()
+
+from dotenv import load_dotenv
+
+# to bring load_dotenv() function to load environment variables from a .env file
+
 from fastapi import FastAPI
 from app.schemas import PostCreateRequest, PostCreateResponse
+
+load_dotenv()  # Load environment variables from .env file
+
+app_name = os.getenv("APP_NAME")
+debug_mode = os.getenv("DEBUG", "false").lower() == "true"
+
 
 app = FastAPI(title="Amazn't Backend")
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Amazn't backend is running"}
+    return {
+        "message": "Amazn't backend is running",
+        "debug": debug_mode,
+    }
 
 
 @app.get("/health")
