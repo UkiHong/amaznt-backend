f3dcd4a656ff359b5156e0d5dce97af13a2f985a
Uki Hong <suhong9507@gmail.com>
2026-04-20 17:07:14 +0100

    user.py edited and Alembic migrations

---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py is used to make the models package, and also to import all models in one place for easier imports in other parts of the application.
+from app.models.user import User
---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from app.database import Base
+from sqlalchemy import Integer, String, Boolean, DateTime, func
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+# Users model for SQLAlchemy, "users" table in DB.
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(20), default="user", nullable=False)
+    bio: Mapped[str | None] = mapped_column(String(150), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
