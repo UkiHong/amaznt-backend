e7d47c58b5a66259cb025f574a552c1ecaacbf8a
Uki Hong <suhong9507@gmail.com>
2026-04-27 18:16:18 +0100

    Added product fail score service test skeleton

---
+++ tests/test_product_fail_score_service.py
@@ -0,0 +1,22 @@
+def test_normalize_score_returns_20_for_1():
+    pass
+
+
+def test_normalize_score_returns_100_for_5():
+    pass
+
+
+def test_normalize_score_raises_value_error_for_invalid_score():
+    pass
+
+
+def test_calculate_final_score_returns_expected_weighted_score():
+    pass
+
+
+def test_get_grade_returns_expected_boundary_grades():
+    pass
+
+
+def test_calculation_version_is_fail_score_v1():
+    pass
